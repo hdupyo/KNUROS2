9463e0963786bb95e3255a050af9887652f6160a
EncyH <DupyoH.id@gmail.com>
2019-06-19 04:00:29 +0900

    Upload file

--- line_detection.cpp
+++ line_detection.cpp
@@ -1,8 +1,8 @@
 //2012097097 홍두
-#include <opencv2/opencv.hpp>  
+#include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <gsl/gsl_fit.h>
-#include <iostream>  
+#include <iostream>
 #include <ros/ros.h>
 #include <cv_bridge/cv_bridge.h>
 #include <iomanip>
@@ -10,7 +10,7 @@
 using namespace cv;
 using namespace std;
 
-//Hough Transform 파라미터
+//허프변환 파라미터
 float rho = 2; // distance resolution in pixels of the Hough grid
 float theta = 1 * CV_PI / 180; // angular resolution in radians of the Hough grid
 float hough_threshold = 15;	 // minimum number of votes(intersections in Hough grid cell)
@@ -18,14 +18,14 @@ float minLineLength = 10; //minimum number of pixels making up a line
 float maxLineGap = 20;	//maximum gap in pixels between connectable line segments
 
 
-//Region - of - interest vertices, 관심 영역 범위 계산시 사용 
+//Region - of - interest vertices, 관심 영역 범위 계산시 사용
 //We want a trapezoid shape, with bottom edge at the bottom of the image
 float trap_bottom_width = 0.85;  // width of bottom edge of trapezoid, expressed as percentage of image width
 float trap_top_width = 0.07;     // ditto for top edge of trapezoid
 float trap_height = 0.4;         // height of the trapezoid expressed as percentage of image height
 
 
-//차선 색깔 범위 
+//차선 색깔 범위
 Scalar lower_black = Scalar(0, 0, 0); //검은 차선 (RGB)
 Scalar upper_black = Scalar(50, 50, 50);
 
@@ -85,32 +85,10 @@ void draw_line(Mat &img_line, vector<Vec4i> lines)
 {
 	if (lines.size() == 0) return;
 
-	/*
-	NOTE : this is the function you might want to use as a starting point once you want to
-	average / extrapolate the line segments you detect to map out the full
-	extent of the lane(going from the result shown in raw - lines - example.mp4
-	to that shown in P1_example.mp4).
-
-	Think about things like separating line segments by their
-	slope((y2 - y1) / (x2 - x1)) to decide which segments are part of the left
-	line vs.the right line.Then, you can average the position of each of
-	the lines and extrapolate to the top and bottom of the lane.
-
-	This function draws `lines` with `color` and `thickness`.
-	Lines are drawn on the image inplace(mutates the image).
-	If you want to make the lines semi - transparent, think about combining
-	this function with the weighted_img() function below
-	*/
-
-	// In case of error, don't draw the line(s)
 	bool draw_right = true;
 	bool draw_left = true;
 	int width = img_line.cols;
 	int height = img_line.rows;
-
-
-	//Find slopes of all lines
-	//But only care about lines where abs(slope) > slope_threshold
 	float slope_threshold = 0.5;
 	vector<float> slopes;
 	vector<Vec4i> new_lines;
@@ -122,27 +100,19 @@ void draw_line(Mat &img_line, vector<Vec4i> lines)
 		int y1 = line[1];
 		int x2 = line[2];
 		int y2 = line[3];
-
-
 		float slope;
-		//Calculate slope
-		if (x2 - x1 == 0) //corner case, avoiding division by 0
-			slope = 999.0; //practically infinite slope
+
+		if (x2 - x1 == 0)
+			slope = 999.0;
 		else
 			slope = (y2 - y1) / (float)(x2 - x1);
 
-
-		//Filter lines based on slope
 		if (abs(slope) > slope_threshold) {
 			slopes.push_back(slope);
 			new_lines.push_back(line);
 		}
 	}
 
-
-
-	// Split lines into right_lines and left_lines, representing the right and left lane lines
-	// Right / left lane lines must have positive / negative slope, and be on the right / left half of the image
 	vector<Vec4i> right_lines;
 	vector<Vec4i> left_lines;
 
@@ -166,15 +136,11 @@ void draw_line(Mat &img_line, vector<Vec4i> lines)
 			left_lines.push_back(line);
 	}
 
-
-	//Run linear regression to find best fit line for right and left lane lines
-	//Right lane lines
 	double right_lines_x[1000];
 	double right_lines_y[1000];
 	float right_m, right_b;
-
-
 	int right_index = 0;
+
 	for (int i = 0; i < right_lines.size(); i++) {
 
 		Vec4i line = right_lines[i];
@@ -210,8 +176,7 @@ void draw_line(Mat &img_line, vector<Vec4i> lines)
 	}
 
 
-
-	// Left lane lines
+	// 왼쪽 라인
 	double left_lines_x[1000];
 	double left_lines_y[1000];
 	float left_m, left_b;
@@ -252,19 +217,15 @@ void draw_line(Mat &img_line, vector<Vec4i> lines)
 
 
 
-	//Find 2 end points for right and left lines, used for drawing the line
+
 	//y = m*x + b--> x = (y - b) / m
 	int y1 = height;
 	int y2 = height * (1 - trap_height);
-
 	float right_x1 = (y1 - right_b) / right_m;
 	float right_x2 = (y2 - right_b) / right_m;
-
 	float left_x1 = (y1 - left_b) / left_m;
 	float left_x2 = (y2 - left_b) / left_m;
 
-
-	//Convert calculated end points from float to int
 	y1 = int(y1);
 	y2 = int(y2);
 	right_x1 = int(right_x1);
@@ -272,16 +233,11 @@ void draw_line(Mat &img_line, vector<Vec4i> lines)
 	left_x1 = int(left_x1);
 	left_x2 = int(left_x2);
 
-
-	//Draw the right and left lines on image
 	if (draw_right)
 		line(img_line, Point(right_x1, y1), Point(right_x2, y2), Scalar(255, 0, 0), 10);
 	if (draw_left)
 		line(img_line, Point(left_x1, y1), Point(left_x2, y2), Scalar(255, 0, 0), 10);
-
 }
-
-
 
 int main(int, char**)
 {
@@ -327,12 +283,12 @@ int main(int, char**)
 	while (1)
 	{
 
-		//1. 원본 영상을 읽어옴 
+		//1. 원본 영상을 읽어옴
 		videoCapture.read(img_bgr);
 		if (img_bgr.empty()) break;
 
 
-		//2. 미리 정한 색 범위 내에 있는 부분만 차선후보로 따로 저장함 
+		//2. 미리 정한 색 범위 내에 있는 부분만 차선후보로 따로 저장함
 		Mat img_filtered;
 		filter_colors(img_bgr, img_filtered);
 
@@ -373,28 +329,25 @@ int main(int, char**)
 
 
 
-		//7. 원본 영상에 6번의 직선을 같이 보여줌 
+		//7. 원본 영상에 6번의 직선을 같이 보여줌
 		addWeighted(img_bgr, 0.8, img_line, 1.0, 0.0, img_annotated);
 
 
-		//8. 결과를 동영상 파일로 기록 
+		//8. 결과를 동영상 파일로 기록
 		writer << img_annotated;
 
 		count++;
 		if (count == 10) imwrite("img_annota1ted.jpg", img_annotated);
 
-		//9. 결과를 화면에 보여줌 
+		//9. 결과를 화면에 보여줌
 		Mat img_result;
 		resize(img_annotated, img_annotated, Size(width*0.7, height*0.7));
 		resize(img_edges, img_edges, Size(width*0.7, height*0.7));
 		cvtColor(img_edges, img_edges, COLOR_GRAY2BGR);
 		hconcat(img_edges, img_annotated, img_result);
-		imshow("차선 감지 영", img_result);
+		imshow("차선 감지 영상 ", img_result);
 
-
-
-
-		if (waitKey(1) == 27) break; //ESC키 누르면 종료  
+		if (waitKey(1) == 27) break; //ESC키 누르면 종료
 	}
 
 
